012d4ae55a18d603e1817492051ab0ef7fc2abbd
Said Bouigherdaine <saidbouigherdaine@gmail.com>
2019-02-09 17:42:36 +0100

    refactor MowerService #WIP

--- src/main/java/fr/xebia/mowItNow/service/MowerService.java
+++ src/main/java/fr/xebia/mowItNow/service/MowerService.java
@@ -2,8 +2,42 @@ package fr.xebia.mowItNow.service;
 
 import fr.xebia.mowItNow.model.Direction;
 import fr.xebia.mowItNow.model.Lawn;
+import fr.xebia.mowItNow.model.Move;
+import fr.xebia.mowItNow.model.Position;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MowerService {
-    public MowerService(Direction northDirection, Lawn lawn) {
+
+    private Position position;
+    private Direction direction;
+    private Lawn lawn;
+
+    public MowerService(final Direction startDirection, final Lawn initLawn) {
+        this.position = new Position(initLawn);
+        this.direction = startDirection;
     }
+
+    public MowerService(final Position startPosition, final Direction startDirection, final Lawn initLawn) {
+        this.setPosition(startPosition);
+        this.getPosition().setLawn(initLawn);
+        this.setDirection(startDirection);
+    }
+
+    public MowerService(final Position startPosition, final Direction startDirection) {
+        this.setPosition(startPosition);
+        this.setDirection(startDirection);
+    }
+
+    public void moveTo(final Move move) {
+
+    }
+
+    @Override
+    public String toString() {
+        return this.getPosition().getX() + " " + this.getPosition().getY() + " " + this.getDirection().getDirection();
+    }
+
 }
--- src/test/java/fr/xebia/mowItNow/service/MowerServiceTest.java
+++ src/test/java/fr/xebia/mowItNow/service/MowerServiceTest.java
@@ -1,9 +1,13 @@
 package fr.xebia.mowItNow.service;
 
+import fr.xebia.mowItNow.model.Position;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static fr.xebia.mowItNow.utils.ConstantTest.LAWN;
-import static fr.xebia.mowItNow.utils.ConstantTest.NORTH_DIRECTION;
+import static fr.xebia.mowItNow.utils.ConstantTest.*;
+import static fr.xebia.mowItNow.utils.ConstantTest.N_0;
+import static fr.xebia.mowItNow.utils.ConstantTest.WEST_DIRECTION;
+import static org.junit.Assert.assertEquals;
 
 public class MowerServiceTest {
 
@@ -19,5 +23,61 @@ public class MowerServiceTest {
     public void init() {
         mowerService = new MowerService(NORTH_DIRECTION, LAWN);
     }
+
+    /**
+     * Initialize position test.
+     */
+    @Test
+    public void initPositionTest() {
+        Position mowerPosition = mowerService.getPosition();
+        assertEquals(mowerPosition.getX(), N_0);
+        assertEquals(mowerPosition.getY(), N_0);
+    }
+
+    /**
+     * Initialize direction test.
+     */
+    @Test
+    public void initDirectionTest() {
+        assertEquals(mowerService.getDirection(), NORTH_DIRECTION);
+    }
+
+    /**
+     * Front command test.
+     */
+    @Test
+    public void frontCommandTest() {
+        mowerService.moveTo(FRONT_MOVE);
+
+        Position expectedPosition = new Position(N_0, N_1);
+        assertEquals(mowerService.getPosition(), expectedPosition);
+    }
+
+    /**
+     * Right command test.
+     */
+    @Test
+    public void rightCommandTest() {
+        mowerService.moveTo(RIGHT_MOVE);
+
+        Position expectedPosition = new Position(N_0, N_0);
+        assertEquals(mowerService.getPosition(), expectedPosition);
+        assertEquals(mowerService.getDirection(), EAST_DIRECTION);
+    }
+
+    /**
+     * Left command test.
+     */
+    @Test
+    public void leftCommandTest() {
+        mowerService.moveTo(LEFT_MOVE);
+
+        Position expectedPosition = new Position(N_0, N_0);
+        assertEquals(mowerService.getPosition(), expectedPosition);
+        assertEquals(mowerService.getDirection(), WEST_DIRECTION);
+    }
+
+
+
 
 }
