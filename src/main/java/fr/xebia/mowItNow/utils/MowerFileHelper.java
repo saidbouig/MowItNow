ad47e478288d43ffc580c0ed2e7e083fb5f1c0ae
Said Bouigherdaine <saidbouigherdaine@gmail.com>
2019-02-09 19:45:49 +0100

    add MowerFileHelperTest

--- src/main/java/fr/xebia/mowItNow/utils/MowerFileHelper.java
+++ src/main/java/fr/xebia/mowItNow/utils/MowerFileHelper.java
@@ -1,13 +1,23 @@
 package fr.xebia.mowItNow.utils;
 
 import fr.xebia.mowItNow.exception.WrongFileException;
+import fr.xebia.mowItNow.model.Direction;
+import fr.xebia.mowItNow.model.Lawn;
+import fr.xebia.mowItNow.model.Move;
+import fr.xebia.mowItNow.model.Position;
+import fr.xebia.mowItNow.service.MowerService;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static fr.xebia.mowItNow.utils.Constant.N_0;
 import static fr.xebia.mowItNow.utils.Constant.N_1;
 import static fr.xebia.mowItNow.utils.Constant.N_2;
 
@@ -39,9 +49,95 @@ public class MowerFileHelper {
             throw new WrongFileException(String.format("Wrong input path file parameter : %s", filePath));
         } else {
             for (int i = 0; i < (count / N_2); i++) {
-                //TODO: Call MowerService and process every move
+                processMoves(initStartPositionFromFile(lines[N_1 + (i * N_2)]),
+                        initStartDirectionFromFile(lines[N_1 + (i * N_2)]), initLawnFromFile(lines[N_0]),
+                        initMovesFromFile(lines[N_2 + (i * N_2)]));
             }
 
+        }
+    }
+
+    private void processMoves(final Position newPosition, final Direction newDirection, final Lawn initLawn,
+                       final List<Move> movesList) {
+        MowerService mowerService = new MowerService(newPosition, newDirection, initLawn);
+        for (Move move : movesList) {
+            mowerService.moveTo(move);
+        }
+
+        System.out.println(mowerService.toString());
+    }
+
+    private List<Move> initMovesFromFile(final String line) throws WrongFileException {
+        String regex = "[AGD]*";
+        if (checkLine(regex, line)) {
+            List<Move> movesTmp;
+            try {
+                movesTmp = new ArrayList<>();
+                for (char c : line.trim().toCharArray()) {
+                    movesTmp.add(Move.to(String.valueOf(c)));
+                }
+                return movesTmp;
+
+            } catch (Exception e) {
+                throw new WrongFileException(String.format("Wrong input file line 3 : %s", line));
+            }
+        } else {
+            throw new WrongFileException(String.format("Wrong input file line 3 : %s", line));
+        }
+
+    }
+
+    private Position initStartPositionFromFile(final String line) throws WrongFileException {
+        String regex = "[0-9]*\\s[0-9]*\\s[ENSW]";
+        if (checkLine(regex, line)) {
+            try {
+                return new Position(Integer.parseInt(line.trim().split(" ")[0]),
+                        Integer.parseInt(line.trim().split(" ")[1]));
+            } catch (Exception e) {
+                throw new WrongFileException(String.format("Wrong input file line 2 : %s", line));
+            }
+        } else {
+            throw new WrongFileException(String.format("Wrong input file line 2 : %s", line));
+        }
+
+    }
+
+    private Direction initStartDirectionFromFile(final String line) throws WrongFileException {
+        String regex = "[0-9]*\\s[0-9]*\\s[ENSW]";
+        if (checkLine(regex, line)) {
+            try {
+                return Direction.of(line.trim().split(" ")[2]);
+            } catch (Exception e) {
+                throw new WrongFileException(String.format("Wrong input file line 2 : %s", line));
+            }
+        } else {
+            throw new WrongFileException(String.format("Wrong input file line 2 : %s", line));
+        }
+
+    }
+
+    private Lawn initLawnFromFile(final String line) throws WrongFileException {
+        String regex = "[0-9]*\\s[0-9]*";
+        if (checkLine(regex, line)) {
+            try {
+                return new Lawn(Integer.parseInt(line.trim().split(" ")[0]),
+                        Integer.parseInt(line.trim().split(" ")[1]));
+            } catch (Exception e) {
+                throw new WrongFileException(String.format("Wrong input file line 1 : %s", line));
+            }
+        } else {
+            throw new WrongFileException(String.format("Wrong input file line 1 : %s", line));
+        }
+    }
+
+    private boolean checkLine(final String regex, final String line) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line.trim());
+        try {
+            matcher.find();
+            return matcher.group(0).equals(line.trim());
+        } catch (Exception e) {
+            return false;
         }
     }
 }
--- src/test/java/fr/xebia/mowItNow/utils/MowerFileHelperTest.java
+++ src/test/java/fr/xebia/mowItNow/utils/MowerFileHelperTest.java
@@ -3,7 +3,9 @@ package fr.xebia.mowItNow.utils;
 import fr.xebia.mowItNow.exception.WrongFileException;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,9 +15,9 @@ class MowerFileHelperTest {
      * Wrong input file test.
      */
     @Test
-    public void wrongFileTest() {
+    public void should_throw_exception_when_wrong_file_content() {
         assertThrows(WrongFileException.class, () -> {
-            new MowerFileHelper(getPath("wrongFile.txt"));
+            new MowerFileHelper(getPath("wrong_file.txt"));
         });
 
     }
@@ -24,12 +26,63 @@ class MowerFileHelperTest {
      * Empty input file test.
      */
     @Test
-    public void emptyFileTwoTest() {
+    public void should_throw_exception_when_empty_file() {
         assertThrows(WrongFileException.class, () -> {
-            new MowerFileHelper(getPath("emptyFile.txt"));
+            new MowerFileHelper(getPath("empty_file.txt"));
         });
 
     }
+
+    /**
+     * un-existed input file test.
+     */
+    @Test
+    public void should_throw_exception_when_file_not_exists() {
+        assertThrows(NullPointerException.class, () -> {
+            getPath("un_existed_file.txt");
+        });
+
+    }
+
+    /**
+     * Right input file test.
+     */
+    @Test
+    public void should_return_correct_result_when_one_line_input() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        new MowerFileHelper(getPath("right_file_1.txt"));
+        assertEquals("1 3 N".trim(), outContent.toString().trim());
+
+    }
+
+    /**
+     * Right input file test.
+     */
+    @Test
+    public void should_return_correct_result_when_multiple_line_input() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        new MowerFileHelper(getPath("right_file_2.txt"));
+        assertEquals("1 3 N\n5 1 E".trim(), outContent.toString().trim());
+
+    }
+
+
+    /**
+     * Right input file test 2.
+     */
+    @Test
+    public void should_return_correct_result_when_mower_crosses_lawn() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        new MowerFileHelper(getPath("right_file_3.txt"));
+        assertEquals("4 4 E".trim(), outContent.toString().trim());
+
+    }
+
+
+
 
     private String getPath(final String nameFile) {
         ClassLoader classLoader = getClass().getClassLoader();
