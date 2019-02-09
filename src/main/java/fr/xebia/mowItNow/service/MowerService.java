9982f7e28f43a57725d97b68f70862473e490b7a
Said Bouigherdaine <saidbouigherdaine@gmail.com>
2019-02-09 18:12:41 +0100

    add moveTo method -> MowerService #WIP

--- src/main/java/fr/xebia/mowItNow/service/MowerService.java
+++ src/main/java/fr/xebia/mowItNow/service/MowerService.java
@@ -1,11 +1,14 @@
 package fr.xebia.mowItNow.service;
 
+import fr.xebia.mowItNow.exception.WrongLawnException;
 import fr.xebia.mowItNow.model.Direction;
 import fr.xebia.mowItNow.model.Lawn;
 import fr.xebia.mowItNow.model.Move;
 import fr.xebia.mowItNow.model.Position;
 import lombok.Getter;
 import lombok.Setter;
+
+import static fr.xebia.mowItNow.utils.Constant.*;
 
 @Getter
 @Setter
@@ -32,6 +35,55 @@ public class MowerService {
     }
 
     public void moveTo(final Move move) {
+
+        switch (move.getCommand()) {
+            case AVANCE:
+                try {
+                    if (Direction.of(NORTH).equals(direction)) {
+                        this.getPosition().incrementY();
+                    } else if (Direction.of(SOUTH).equals(direction)) {
+                        this.getPosition().decrementY();
+                    } else if (Direction.of(EAST).equals(direction)) {
+                        this.getPosition().incrementX();
+                    } else {
+                        this.getPosition().decrementX();
+                    }
+                } catch (WrongLawnException e) {
+                    assert e.getMessage().equals(String.format("Position (%s, %s) out of bounds: ",
+                            this.getPosition().getX(), this.getPosition().getY()));
+                }
+
+                break;
+
+            case DROITE:
+                if (Direction.of(NORTH).equals(direction)) {
+                    this.setDirection(Direction.of(EAST));
+                } else if (Direction.of(SOUTH).equals(direction)) {
+                    this.setDirection(Direction.of(WEST));
+                } else if (Direction.of(EAST).equals(direction)) {
+                    this.setDirection(Direction.of(SOUTH));
+                } else {
+                    this.setDirection(Direction.of(NORTH));
+                }
+
+                break;
+
+            case GAUCHE:
+                if (Direction.of(NORTH).equals(direction)) {
+                    this.setDirection(Direction.of(WEST));
+                } else if (Direction.of(SOUTH).equals(direction)) {
+                    this.setDirection(Direction.of(EAST));
+                } else if (Direction.of(EAST).equals(direction)) {
+                    this.setDirection(Direction.of(NORTH));
+                } else {
+                    this.setDirection(Direction.of(SOUTH));
+                }
+                break;
+
+            default:
+                break;
+
+        }
 
     }
 
